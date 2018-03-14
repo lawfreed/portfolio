a898fba69a6b98eb8d2ce4ca536b70f0f880cdc0
Mark Lawfreed <lawfreed@protonmail.com>
2018-03-15 02:11:28 +0800

    Use "asset()" function for CSS and JS links and resources

--- resources/views/partials/csslinks.blade.php
+++ resources/views/partials/csslinks.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="{{ URL::asset('/css/semantic.min.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('/css/popup.min.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('/css/hover-min.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('/css/custom.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/semantic.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/popup.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/hover-min.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/custom.css') }}">
--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="{{ URL::asset('/js/jquery-3.2.1.min.js') }}"></script>
-<script type="text/javascript" src="{{ URL::asset('/js/semantic.min.js') }}"></script>
-<script type="text/javascript" src="{{ URL::asset('/js/popup.min.js') }}"></script>
-<script type="text/javascript" src="{{ URL::asset('/js/custom.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/js/jquery-3.2.1.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/js/semantic.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/js/popup.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/js/custom.js') }}"></script>
